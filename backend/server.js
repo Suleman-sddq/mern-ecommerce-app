cfbc9e9c95b2eaf56cbe09a97c6644431feb5e44
M.Suleman <sulemansddq@gmail.com>
2023-08-06 14:40:01 +0500

    rechecck

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
    const __dirname = path.resolve();
    app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
-   app.use(express.static(path.resolve(__dirname, '../', 'frontend', 'build')));
+   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
    );
