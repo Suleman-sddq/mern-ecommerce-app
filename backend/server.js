2319088f84cf0e3e5d0ced8b91ce5fc6c2dab791
M.Suleman <sulemansddq@gmail.com>
2023-08-06 14:01:20 +0500

    added vercel.json in root

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,9 @@ if (process.env.NODE_ENV === 'production') {
    const __dirname = path.resolve();
    app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
-   app.use(express.static(path.join(__dirname, '/frontend/build')));
+   app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
 
-   app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
    );
 } else {
    const __dirname = path.resolve();
