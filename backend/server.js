441ff0a280972654d8b502ec2ccba3c16e97baf7
M.Suleman <sulemansddq@gmail.com>
2023-09-05 12:57:51 +0500

    updated static uploads folder

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,15 @@ app.use('/api/upload', uploadRoutes);
 if (process.env.NODE_ENV === 'production') {
 
    const __dirname = path.resolve();
-   app.use('/uploads', express.static(path.join(__dirname, './uploads')));
+   app.use('/uploads', express.static('/var/data/uploads'));
 
    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
    );
+
+
+
 } else {
    const __dirname = path.resolve();
    app.use('/uploads', express.static(path.join(__dirname, './uploads')));
