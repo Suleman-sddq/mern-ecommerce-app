d193e232bf6760843029444cf8b85ca6fb66d0bc
M.Suleman <sulemansddq@gmail.com>
2023-09-05 13:23:44 +0500

    removed .. from l-43

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,10 @@ app.use('/api/upload', uploadRoutes);
 if (process.env.NODE_ENV === 'production') {
 
    const __dirname = path.resolve();
+
    app.use('/uploads', express.static('/var/data/uploads'));
 
-
-   app.use(express.static(path.join(__dirname, '../frontend/build')));
+   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
    app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 
